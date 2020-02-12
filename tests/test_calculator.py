d38d0bbfb7210fda47aafcfc28b9e861d3d6b3e6
Rajan Patil <rajan.rb@gmail.com>
2020-02-12 21:20:53 +0000

    Test refactored

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -2,34 +2,26 @@ from calc.calculator import Calculator
 import pytest
 
 
-def test_add_2_plus_3():
-    calc = Calculator()
-    assert calc.add(2, 3) == 5
+class TestCalculator:
 
+    def setup(self):
+        self.calc = Calculator()
 
-def test_add_6_plus_8():
-    calc = Calculator()
-    assert calc.add(6, 8) == 14
+    def test_addition_2_plus_3(self):
+        assert self.calc.add(2, 3) == 5
 
+    def test_addition_6_plus_8(self):
+        assert self.calc.add(6, 8) == 14
 
-def test_subtraction_4_minus_3():
-    calc = Calculator()
-    assert calc.sub(4, 3) == 1
+    def test_subtraction_4_minus_3(self):
+        assert self.calc.sub(4, 3) == 1
 
+    def test_subtraction_9_minus_2(self):
+        assert self.calc.sub(9, 2) == 7
 
-def test_subtraction_9_minus_2():
-    calc = Calculator()
-    assert calc.sub(9, 2) == 7
+    def test_division_9_divide_by_3(self):
+        assert self.calc.div(9, 3) == 3
 
-
-def test_division_9_divide_by_3():
-    calc = Calculator()
-
-    assert calc.div(9, 3) == 3
-
-
-def test_division_error_divide_by_zero():
-    calc = Calculator()
-
-    with pytest.raises(ZeroDivisionError):
-        calc.div(5, 0)
+    def test_division_error_divide_by_zero(self):
+        with pytest.raises(ZeroDivisionError):
+            self.calc.div(5, 0)
